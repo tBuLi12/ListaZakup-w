3600b883214a6d7ddf8e97cfe0ec74ab393e282d
Maciej Szczepański <01142079@pw.edu.pl>
2021-05-29 21:27:14 +0200

    Added more basic tests.

--- ProductUnitTest/ProductUnitTest.cpp
+++ ProductUnitTest/ProductUnitTest.cpp
@@ -11,7 +11,7 @@ namespace ProductUnitTest
 	{
 	public:
 		
-		TEST_METHOD(FoodConstructor)
+		TEST_METHOD(Constructor)
 		{
 			vector<double> nutrition{ 76.0, 0.1, 17.0, 2.2, 2.0, 0.006 };
 			Food f("potato", 50, 1000, nutrition);
@@ -24,6 +24,34 @@ namespace ProductUnitTest
 			Assert::AreEqual(2.2, f.get_fibre_per_100g(), 0.001);
 			Assert::AreEqual(2.0, f.get_protein_per_100g(), 0.001);
 			Assert::AreEqual(0.006, f.get_salt_per_100g(), 0.001);
+		}
+
+		TEST_METHOD(get_name)
+		{
+			vector<double> nutrition{ 76.0, 0.1, 17.0, 2.2, 2.0, 0.006 };
+			Food f("potato", 50, 1000, nutrition);
+			Assert::IsTrue("potato" == f.get_name());
+		}
+
+		TEST_METHOD(get_price)
+		{
+			vector<double> nutrition{ 76.0, 0.1, 17.0, 2.2, 2.0, 0.006 };
+			Food f("potato", 50, 1000, nutrition);
+			Assert::AreEqual(50, f.get_price());
+		}
+
+		TEST_METHOD(get_weight)
+		{
+			vector<double> nutrition{ 76.0, 0.1, 17.0, 2.2, 2.0, 0.006 };
+			Food f("potato", 50, 1000, nutrition);
+			Assert::AreEqual(1000, f.get_weight());
+		}
+
+		TEST_METHOD(get_kcal_per_100g)
+		{
+			vector<double> nutrition{ 76.0, 0.1, 17.0, 2.2, 2.0, 0.006 };
+			Food f("potato", 50, 1000, nutrition);
+			Assert::AreEqual(76.0, f.get_kcal_per_100g(), 0.001);
 		}
 	};
 }
