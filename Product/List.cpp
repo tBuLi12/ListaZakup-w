0639ce415df55f6bdf27f568d2773def1f461b6f
Mateusz Łukasz Krakowski <01158769@pw.edu.pl>
2021-06-03 20:28:54 +0200

    list early SoD

--- Product/List.cpp
+++ Product/List.cpp
@@ -1,14 +1,32 @@
 #include "List.h"
+#include<string>
+#include<vector>
 
-using namespace std;
 
 
-string Product::get_list_name() const noexcept
+std::string List::get_list_name() const noexcept
 {
 	return list_name;
 }
 
-int Product::get_total_price() const noexcept
+int List::get_total_price() const noexcept
 {
 	return total_price_gr;
+}
+
+List::List(std::string list_name, std::vector<Product*> product_vector) {
+
+	this->list_name = list_name;
+	this->products = product_vector;
+
+	int total_price;
+	int total_weight;
+
+	
+	for (auto product : product_vector) {
+		total_price += product->get_price();
+		total_weight += product->get_weight();
+	}
+	this->total_price_gr = total_price;
+	this->total_weight_grams = total_weight;
 }
--- Product/List.h
+++ Product/List.h
@@ -1,14 +1,21 @@
 #pragma once
 #include<string>
-#include<array>
-
-class Product
+#include<vector>
+#include "Product.h"
+class List
 {
 protected:
 	std::string list_name;
+
 	int total_price_gr;
+	int total_weight_grams;
+
+	std::vector<Product*> products;
+	
 
 public:
+	List(std::string list_name, std::vector<Product*> product_vector) noexcept;
 	std::string get_list_name() const noexcept;
 	int get_total_price() const noexcept;
+
 };
